01f570649334fad98c821a238de9e0a7bebe0cb9
Yusuf-ELSwaify <yusufelswaify12@gmail.com>
2023-07-15 04:38:19 +0300

    Calculate the sum and average from a file project applied the code structure

--- week-5/sum_of_numbers_in_file/src/main/java/job_hacker/exercise/App.java
+++ week-5/sum_of_numbers_in_file/src/main/java/job_hacker/exercise/App.java
@@ -1,48 +1,28 @@
 package job_hacker.exercise;
 
 import java.io.*;
-import java.net.URL;
-import java.util.InputMismatchException;
-import java.util.Scanner;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
-
+    public static final String FILE_NAME = "Scores.txt";
+    private static final String URL = "http://liveexample.pearsoncmg.com/data/Scores.txt";
 
     private static void print(File file, OperationEnum operation){
-        try(Scanner scanner = new Scanner(file)) {
-            int value = 0;
-            if (operation == OperationEnum.Sum)
-                value = Helper.getSumOfNumbers(scanner);
-            else if (operation == OperationEnum.Average)
-                value = Helper.getAverageOfNumbers(scanner);
 
-            System.out.println(operation.name() + " of numbers in file is "+ value);
-        } catch (FileNotFoundException e) {
-            System.out.println("File " + Helper.FILE_NAME + " Not Found");
-            System.out.println(e.getMessage());
-        }
+        int value = 0;
+        if (operation == OperationEnum.Sum)
+            value = Helper.getSum(file);
+        else if (operation == OperationEnum.Average)
+            value = Helper.getAverage(file);
+
+        System.out.println(operation.name() + " of numbers in file is "+ value);
     }
 
     public static void main( String[] args )
     {
-        try {
-            Helper.readFromWebToFile();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-        try {
-            File file = new File(App.class.getResource("/" + Helper.FILE_NAME).getFile());
-            print(file, OperationEnum.Sum);
-            print(file, OperationEnum.Average);
-
-        } catch (NullPointerException nullPointerException){
-            System.out.println("File " + Helper.FILE_NAME + " Not Found");
-            System.out.println(nullPointerException.getMessage());
-        }
+        File file = Helper.readFromWebToFile(URL);
+        print(file, OperationEnum.Sum);
+        print(file, OperationEnum.Average);
     }
 }
--- week-5/sum_of_numbers_in_file/src/main/java/job_hacker/exercise/Helper.java
+++ week-5/sum_of_numbers_in_file/src/main/java/job_hacker/exercise/Helper.java
@@ -1,59 +1,66 @@
 package job_hacker.exercise;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
+import java.nio.charset.StandardCharsets;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Helper {
-    public static final String FILE_NAME = "Scores.txt";
-    private static final String URL = "http://liveexample.pearsoncmg.com/data/Scores.txt";
-    public static void readFromWebToFile() throws IOException {
-        URL scoreUrl = new URL(URL);
-        Scanner scanner = new Scanner(scoreUrl.openStream());
-        BufferedWriter writer = new BufferedWriter(new FileWriter("src/main/resources/" + "Scores.txt"));
-        BufferedWriter targetWriter = new BufferedWriter(new FileWriter("target/classes/" + "Scores.txt"));
 
-        while (scanner.hasNextLine()) {
-            String line = scanner.nextLine();
-            writer.write(line);
-            targetWriter.write(line);
-            writer.newLine();
-            targetWriter.newLine();
+    public static File readFromWebToFile(String url) {
+        Scanner scanner = null;
+        try {
+            URL scoreUrl = new URL(url);
+            scanner = new Scanner(scoreUrl.openStream(), StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            System.out.println("Cant open connection with " + url);
+            throw new RuntimeException(e);
         }
+
+        PrintWriter writer = null;
+        File file = new File( App.FILE_NAME);
+        try {
+            writer = new PrintWriter(file);
+        } catch (IOException e) {
+            System.out.println("cant create file " + file.getName());
+            throw new RuntimeException(e);
+        }
+
+        while (scanner.hasNextLine())
+            writer.println(scanner.nextLine());
 
         scanner.close();
         writer.close();
-        targetWriter.close();
-    }
-    private static int getAverageOrSum(Scanner scanner, OperationEnum operationEnum){
-        int wordCount = 0, sum = 0;
 
-        while (scanner.hasNext()) {
-            try {
-                sum += scanner.nextInt();
-                wordCount++;
+        return file;
+    }
+    private static int getAverageOrSum(File file, OperationEnum operationEnum){
+        try(Scanner scanner = new Scanner(file)) {
+            int wordCount = 0, sum = 0;
+
+            while (scanner.hasNext()) {
+                try {
+                    sum += scanner.nextInt();
+                    wordCount++;
+                }
+                catch (InputMismatchException e){
+                    System.out.println("Invalid number in the file-> " + scanner.next() + ".");
+                }
             }
-            catch (InputMismatchException e){
-                System.out.println("Invalid number in the file-> " + scanner.next() + ".");
-            }
+            if (wordCount == 0)
+                return 0;
+            return operationEnum == OperationEnum.Average ? (sum / wordCount) : sum;
+        } catch (FileNotFoundException e) {
+            System.out.println("File " + file.getName() + " Not Found");
+            throw new RuntimeException(e);
         }
-        if (wordCount == 0)
-            return 0;
-        return operationEnum == OperationEnum.Average ? (sum / wordCount) : sum;
     }
-    public static int getAverageOfNumbers(Scanner scanner){
-        return getAverageOrSum(scanner, OperationEnum.Average);
+    public static int getAverage(File file){
+        return getAverageOrSum(file, OperationEnum.Average);
     }
-    public static int getSumOfNumbers(Scanner scanner){
-        return getAverageOrSum(scanner, OperationEnum.Sum);
+    public static int getSum(File file){
+        return getAverageOrSum(file, OperationEnum.Sum);
     }
 
 }
